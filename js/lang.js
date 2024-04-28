eb179a2196b80affed44bf2e27b005a01cd6e211
longopy <javier_longobardo@hotmail.com>
2024-04-28 10:51:01 +0200

    Add uk language and modify attributions

--- js/lang.js
+++ js/lang.js
@@ -1,40 +1,51 @@
-import $ from 'jquery';
+import $ from "jquery";
 
 // Styles
-import "../css/lang.css"
+import "../css/lang.css";
+
+const LANGS = ["en", "es", "uk"];
 
 function detectLanguage() {
-    if (!localStorage.hasOwnProperty('lang')){
-        const langs = ['en', 'es'];
-        const browserLang = (navigator.language || navigator.userLanguage).split('-')[0];
-        const lang = langs.includes(browserLang) ? browserLang : 'en';
-        localStorage.setItem('lang', lang);
-        document.documentElement.setAttribute("lang", lang);
-    }
+  if (!localStorage.hasOwnProperty("lang")) {
+    const browserLang = (navigator.language || navigator.userLanguage).split(
+      "-"
+    )[0];
+    const lang = LANGS.includes(browserLang) ? browserLang : "en";
+    localStorage.setItem("lang", lang);
+    document.documentElement.setAttribute("lang", lang);
+  }
 }
 
 function loadLangButton() {
-    const langBtn = document.getElementById('lang-btn');
-    const lang = getTheOtherOption();
-    langBtn.innerHTML = lang.toUpperCase();
+  const langBtns = document.getElementById("lang-btns");
+  const other_langs = getTheOthersOptions();
+  other_langs.forEach((lang) => {
+    const btn = document.createElement("button");
+    btn.classList.add(
+      "dropdown-item",
+      "rounded",
+      "text-light",
+      "change-language-btn"
+    );
+    btn.href = "#";
+    btn.value = lang;
+    btn.innerHTML = lang.toUpperCase();
+    langBtns.appendChild(btn);
+  });
 }
 
-function getTheOtherOption(){
-    const lang = localStorage.getItem('lang')
-    if (lang === 'en') {
-        return 'es';
-    }else{
-        return 'en';
-    }
+function getTheOthersOptions() {
+  const lang = localStorage.getItem("lang");
+  return LANGS.filter((l) => l !== lang);
 }
-$(function (){
-    detectLanguage();
-    loadLangButton();
-    $('#change-language-btn').on('click', function (e){
-        e.preventDefault();
-        const lang = getTheOtherOption();
-        document.documentElement.setAttribute("lang", lang);
-        localStorage.setItem('lang', lang);
-        window.location.reload();
-    });
+$(function () {
+  detectLanguage();
+  loadLangButton();
+  $(".change-language-btn").on("click", function (e) {
+    e.preventDefault();
+    const lang = $(this).val();
+    document.documentElement.setAttribute("lang", lang);
+    localStorage.setItem("lang", lang);
+    window.location.reload();
+  });
 });
